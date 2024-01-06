a13e13ee3084f43c4015c2e39e7744fed4223b62
davi <daviferreiradev@gmail.com>
2024-01-06 15:34:24 -0300

    feat: Warning messages

---
+++ src/components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="message-container">
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Message',
+    props: {
+        msg: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.message-container {
+    background-color: #f5f5f5;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px 0;
+}
+</style>
